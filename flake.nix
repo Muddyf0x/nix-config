023d727fccb78db0f6de7e1e0844b0fcabc5b3bf
Muddyf0x <dev.muddy@tuta.com>
2026-02-27 18:47:54 +0100

    changed hosts

--- flake.nix
+++ flake.nix
@@ -1,15 +1,59 @@
+
 {
-  description = "A very basic flake";
+  description = "Muddy's Nixos-config flake";
+# Based on quanchobi.io 's config
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+# Todo Add: 
+# impermanence: clean up non permanent files on reboot
+# nixvirt: Declerativ vm managment with libvirt
+# stylix: styl everything in one place 
+# silentSDDM: good looking login manager
+# Disko: Declerativ partitioning and formationg 
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, home-manager, agenix, ... }: {
+    nixosConfigurations = {
+      # Desktop configuration
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./host/desktop.nix
+          home-manager.nixosModules.home-manager
+          agenix.nixosModules.default
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.anderson = import ./home/muddy;
+          }
+        ];
+      };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      # Laptop configuration
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./host/laptop.nix
+          home-manager.nixosModules.home-manager
+          agenix.nixosModules.default
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.anderson = import ./home/muddy;
+          }
+        ];
+      };
+    };
   };
 }
