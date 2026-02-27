08491ee83458e3e8ca356faf7b9a10f311e58f88
Muddyf0x <dev.muddy@tuta.com>
2026-02-27 23:39:23 +0100

    init home file

---
+++ home/muddy/home.nix
@@ -0,0 +1,14 @@
+{ inputs, config, pkgs, ...}: 
+{
+  imports = [
+    ./cli
+  ];
+  config = {
+    home = {
+      username = "muddy"; 
+      homeDirectory = "/home/muddy";
+      stateVersion = "25.11";
+    };
+    programs.home-manager.enable = true;
+  };
+}
