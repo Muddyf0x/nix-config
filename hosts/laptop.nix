1e57c2aab08b5e2b979e56c389095d5fc6fbcd01
Muddyf0x <dev.muddy@tuta.com>
2026-02-27 21:54:47 +0100

    add modules and hardware config to laptop

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,7 +1,18 @@
 { config, pkgs, ... }: {
   imports = [
-    ./common    
+    ./common
+    ./opt/de/plasma6
+    ./opt/gaming
+    ./opt/bluetooth
+    ./hardware/laptop.nix
   ];
-
+  # Unique to Host
   networking.hostName = "lappytop";
+
+  # TODO unifi bootloader config -> 
+  # combine with switching to btrfs with impermanance 
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 }
