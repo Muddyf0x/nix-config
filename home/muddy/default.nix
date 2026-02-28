38e127d348be257d185d19743390409a6f29a404
Muddyf0x <dev.muddy@tuta.com>
2026-02-28 10:46:39 +0100

    added missing user config

--- home/muddy/default.nix
+++ home/muddy/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
 #    ./cli
+    ./user.nix
   ];
   config = {
     home = {
---
+++ home/muddy/user.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  user.users.muddy = {
+    isNormalUser = true;
+    description = "muddy";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+}
